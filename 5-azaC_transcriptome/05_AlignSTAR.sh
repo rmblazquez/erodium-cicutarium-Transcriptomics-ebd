95cf488026e7c89b9748d526764307e3556e3b43
rmblazquez <rmartinblazquez@gmail.com>
2025-12-03 20:15:47 +0100

    Update 05_AlignSTAR.sh
    Changed STAR parameters for PE Illumina reads (lines 12 to 20)

--- 5-azaC_transcriptome/05_AlignSTAR.sh
+++ 5-azaC_transcriptome/05_AlignSTAR.sh
@@ -11,15 +11,13 @@ mkdir -p $projectDir/results/star/$line.STAR;
 STAR --runThreadN 4 \
   --genomeDir $projectDir/data/genome/STAR-2.4.2a-R64_Ecic_Index \
   --readFilesIn $projectDir/results/trimmomatic/$line\_1.trimmed.paired.fastq.gz $projectDir/results/trimmomatic/$line\_2.trimmed.paired.fastq.gz \
-  --readFilesCommand gunzip -c \
   --sjdbGTFfile $projectDir/data/genome/GCA_046563565.1_EBD_Ecic_2.1_genomic.gtf \
-  --sjdbOverhang 99 \
-  --limitGenomeGenerateRAM 5900000000 \
-  --outSAMtype BAM SortedByCoordinate \
   --outFileNamePrefix $projectDir/results/star/$line.STAR/$line.STAR \
-  --quantMode GeneCounts \
-  --twopassMode Basic;
- # Do not let STAR use more than 5.9 GB of memory
+  --readFilesCommand gunzip -c \
+  --quantMode TranscriptomeSAM GeneCounts \
+  --twopassMode Basic \
+  --outSAMunmapped Within  \
+  --outSAMtype BAM SortedByCoordinate;
  # Output your alignment file in BAM format, sorted by coordinate
  # Give your output files a UNIQUE name/prefix (${line} also works)
  # Gene counting for generating gene count tables
